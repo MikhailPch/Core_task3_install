f13e2dcd3a39f209f09dd4d66f3922c1a58f0871
mikh74 <pachurin74@yandex.ru>
2023-07-16 20:14:10 +0500

    Modification #1

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,64 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
-    static StringBuilder log = new StringBuilder();
 
     public static void main(String[] args) {
 
-        File file1 = new File("D://Games/src");
-        makeDir(file1);
-        File file2 = new File("D://Games/res");
-        makeDir(file2);
-        File file3 = new File("D://Games/savegames");
-        makeDir(file3);
-        File file4 = new File("D://Games/temp");
-        makeDir(file4);
-        File file5 = new File("D://Games/src/main");
-        makeDir(file5);
-        File file6 = new File("D://Games/src/test");
-        makeDir(file6);
-        File file7 = new File("D://Games/src/main/Main.java");
-        createFile(file7);
-        File file8 = new File("D://Games/src/main/Utils.java");
-        createFile(file8);
-        File file9 = new File("D://Games/res/drawables");
-        makeDir(file9);
-        File file10 = new File("D://Games/res/vectors");
-        makeDir(file10);
-        File file11 = new File("D://Games/res/icons");
-        makeDir(file11);
-        File file12 = new File("D://Games/temp/temp.txt");
-        createFile(file12);
+        StringBuilder log = new StringBuilder();
 
-        try (FileWriter writer = new FileWriter("D://Games/temp/temp.txt")) {
-            writer.write(log.toString());
-            System.out.println(log.toString());
+        List<String> listDir = new ArrayList<>();
+        List<String> listFiles = new ArrayList<>();
+        listDir.add("D://Games/src");
+        listDir.add("D://Games/res");
+        listDir.add("D://Games/savegames");
+        listDir.add("D://Games/temp");
+        listDir.add("D://Games/src/main");
+        listDir.add("D://Games/src/test");
+        listFiles.add("D://Games/src/main/Main.java");
+        listFiles.add("D://Games/src/main/Utils.java");
+        listDir.add("D://Games/res/drawables");
+        listDir.add("D://Games/res/vectors");
+        listDir.add("D://Games/res/icons");
+        listFiles.add("D://Games/temp/temp.txt");
+
+        for (String i : listDir
+        ) {
+            makeDir(i, log);
+        }
+        for (String i : listFiles
+        ) {
+            createFile(i, log);
+        }
+
+        writeLog("D://Games/temp/temp.txt", log);
+
+    }
+
+    private static void makeDir(String pathDir, StringBuilder sb) {
+        if (new File(pathDir).mkdir()) {
+            sb.append("Каталог " + pathDir + " создан\n");
+        }
+    }
+
+    private static void createFile(String pathFile, StringBuilder sb) {
+        try {
+            if (new File(pathFile).createNewFile()) {
+                sb.append("Файл " + pathFile + " создан\n");
+            }
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    static void makeDir(File file) {
-        if (file.mkdir()) {
-            log.append("Каталог " + file.getName() + " создан\n");
-        }
-    }
-
-    static void createFile(File file) {
-        try {
-            if (file.createNewFile()) {
-                log.append("Файл " + file.getName() + " создан\n");
-            }
+    private static void writeLog(String pathFileLog, StringBuilder sb) {
+        try (FileWriter writer = new FileWriter(pathFileLog)) {
+            writer.write(sb.toString());
         } catch (IOException ex) {
-            System.out.println(log.toString());
+            System.out.println(ex.getMessage());
         }
     }
 }
